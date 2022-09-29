d13b37dcaf5d205769f3bd52b694225c07c9eef2
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-09-28 23:07:50 -0700

    completed cards

--- student-files/script.js
+++ student-files/script.js
@@ -10,10 +10,6 @@ let profileURL = "https://randomuser.me/api/";
 let gallery = document.querySelector(".gallery");
 
 function getDetails(data) {
-  //   let information = data.results[0]; //able to use dot notation
-  //   console.log(information.picture.thumbnail);
-
-  //   for (let i = 0; i < 12; i++)
   let newCard = document.createElement("div");
   newCard.className = "card";
 
@@ -23,8 +19,8 @@ function getDetails(data) {
 </div>
 <div class="card-info-container">
 <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
-<p class="card-text">email</p>
-<p class="card-text cap">city, state</p>
+<p class="card-text">${data.email}</p>
+<p class="card-text cap">${data.location.city}, ${data.location.state}</p>
 </div>
 </div>
 
@@ -39,14 +35,3 @@ for (let i = 0; i < 12; i++) {
     .then((data) => data.results[0])
     .then((data) => getDetails(data));
 }
-
-//   <div class="card">
-//                     <div class="card-img-container">
-//                         <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-//                     </div>
-//                     <div class="card-info-container">
-//                         <h3 id="name" class="card-name cap">first last</h3>
-//                         <p class="card-text">email</p>
-//                         <p class="card-text cap">city, state</p>
-//                     </div>
-//                 </div>
