6fb4174ebc59dc3faab047490f9e8c3b571cad7a
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-09-30 10:44:29 -0700

    working project
    -modal container pop up working
    -modal display removed

--- student-files/script-test.js
+++ student-files/script-test.js
@@ -10,7 +10,7 @@ fetch("https://randomuser.me/api/?results=12&nat=us")
   .then((data) => data.results)
   .then((data) => (profileList = data))
   .then(() => getDetails(profileList))
-  .then(() => modalPopup(profileList));
+ 
 
 let gallery = document.querySelector(".gallery");
 let body = document.querySelector("body");
@@ -28,7 +28,7 @@ function getDetails(profileList) {
        <img class="card-img" src="${profileList[i].picture.large}" alt="profile picture">
    </div>
    <div class="card-info-container">
-       <h3 id="name" class="card-name cap">${profileList[i].name.first} last</h3>
+       <h3 id="name" class="card-name cap">${profileList[i].name.first} ${profileList[i].name.last}</h3>
        <p class="card-text">${profileList[i].email}</p>
        <p class="card-text cap">${profileList[i].location.city}, ${profileList[i].location.state}</p>
    </div>
@@ -37,51 +37,65 @@ function getDetails(profileList) {
    `;
     gallery.insertAdjacentHTML("beforeend", html);
   }
+  gallery.addEventListener("click", (e) => {
 
-  //modalPopup(profileList);
+    //let clickableCards = document.querySelectorAll(".card");
+    if (e.target !== gallery) {
+     
+
+      let info = e.target.closest(".card");
+      info = info.children[1].children[0].textContent;
+      for (let i = 0; i < profileList.length; i++) {
+        if (info === `${profileList[i].name.first} ${profileList[i].name.last}` ) {
+          modalPopup(profileList[i]);
+        }
+      }
+    }
+  });
+  
 }
 
 /**
  *
- * @param {array} profileList
+ * @param {array} profile
  */
-function modalPopup(profileList) {
-  for (let i = 0; i < profileList.length; i++) {
+function modalPopup(profile) {
     html = `<div class="modal-container">
     <div class="modal">
         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
         <div class="modal-info-container">
             <img class="modal-img" src="${
-              profileList[i].picture.large
+              profile.picture.large
             }" alt="profile picture">
             <h3 id="name" class="modal-name cap">${
-              profileList[i].name.first
-            }</h3>
-            <p class="modal-text">${profileList[i].email}</p>
-            <p class="modal-text cap">${profileList[i].location.city}</p>
+              profile.name.first
+            } ${profile.name.last}</h3>
+            <p class="modal-text">${profile.email}</p>
+            <p class="modal-text cap">${profile.location.city}</p>
             <hr>
-            <p class="modal-text">${profileList[i].cell}</p>
-            <p class="modal-text">${profileList[i].location.street.number} ${
-      profileList[i].location.street.name
-    }, ${profileList[i].location.city}, ${profileList[i].location.state} ${
-      profileList[i].location.postcode
+            <p class="modal-text">${profile.cell}</p>
+            <p class="modal-text">${profile.location.street.number} ${
+      profile.location.street.name
+    }, ${profile.location.city}, ${profile.location.state} ${
+      profile.location.postcode
     }</p>
             <p class="modal-text">Birthday: ${birthdayReformatter(
-              profileList[i].dob.date
+              profile.dob.date
             )}</p>
         </div>
     </div>
 </div>`;
-  }
+  
   body.insertAdjacentHTML("beforeend", html);
   let modalContainer = document.querySelector(".modal-container");
-  //modalContainer.style.display = "none";
+ 
 
-  modalContainer.addEventListener("click", (e) => {
-    if (e.target.tagName === "BUTTON" || e.target.tagName === "STRONG") {
-      modalContainer.style.display = "none";
-    }
-  });
+  let closeButton = document.querySelector('.modal-close-btn');
+  closeButton.addEventListener("click", (e)=> {
+   modalContainer.remove();
+})
+
+ 
 }
 
 /**
@@ -96,15 +110,3 @@ function birthdayReformatter(birthdayString) {
   birthday = `${birthday[1]}/${birthday[2]}/${birthday[0]}`;
   return birthday;
 }
-
-gallery.addEventListener("click", (e) => {
-  let clickableCards = document.querySelectorAll(".card");
-  if (e.target !== gallery) {
-    console.log("this is working");
-
-    for (let i = 0; i < clickableCards.length; i++) {
-      if (clickableCards[i]) {
-      }
-    }
-  }
-});
